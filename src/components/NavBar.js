cf115b8efdb551de2ef5536c3fe8ca9d84e3863a
khnRaheel <raheelisrail@gmail.com>
2025-02-12 15:34:36 +0400

    second commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#linkedin.com/in/muhammad-raheel-khan-720046175 "><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/muhammad-raheel-khan-720046175"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
